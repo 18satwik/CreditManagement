8db74752d02fe4d335e905ddc56346a7eb3147bd
18satwik <41579386+18satwik@users.noreply.github.com>
2020-06-26 20:00:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
     require 'dbConnect.php';
-    $query="SELECT * FROM users";
-    $result=  mysqli_query($con, $query);
-    
+    require 'header.php';
 ?>
-
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -22,52 +19,61 @@ and open the template in the editor.
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" type="text/css" href="index.css">
-    </head>
-    <body>  <nav class="nav navbar-fixed-top navbar-inverse">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Home</a>
-    </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-right">
-          </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+
+<div class="jumbotron">
         <div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title"></h3>
-  </div>
-  <div class="panel-body">
-       <table class="table table-hover table-striped table-bordered ">
-   <caption><h4><span class="glyphicon glyphicon-leaf" ></span></h4></caption>
-    <tr class="info"><th>S.No</th><th>Name</th> <th> Email </th><th>Credit </th><th>
-  </div>
+            <div class="panel-body"><h1><center>Credit Management System</center></h1></div>
+        </div>
+    </div>
+<div class="row">
+    <div class="col-xl-3 col-sm-6 mb-3">
+        <div class="card-group">
+        <div class="card" style="margin-left: 20px;border-radius: 20px;">
+            <div class="card text-white bg-success">
+                <div class="card-body">
+                    <div class="card-body-icon">
+                    <center><i class="fa fa-fw fa-address-book" style="padding-top: 6px;"></i></center>
+                    </div>
+                    <center>
+                    <div><h3><b>Users</b></h3></div>
+                    </center>
+                </div>
+                <a href="viewuser.php">
+                    <span class="float-left"><h5><b>View all users</b></h5></span>
+                    <span class="float-right">
+                    <i class="fa fa-angle-right fa-3x"></i>
+                    </span>
+                </a>
+            </div>
+        </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-sm-6 mb-3">
+        <div class="card-group">
+        <div class="card" style="margin-left: 20px;border-radius: 20px;">
+            <div class="card text-white bg-success">
+                <div class="card-body">
+                    <div class="card-body-icon">
+                    <center><i class="fa fa-fw fa-file" style="padding-top: 6px;"></i></center>
+                    </div>
+                    <center>
+                    <div><h3><b>Records</b></h3></div>
+                    </center>
+                </div>
+                <a href="transferrecord.php">
+                    <span class="float-left"><h5><b>Transfer Records</b></h5></span>
+                    <span class="float-right">
+                    <i class="fa fa-angle-right fa-3x"></i>
+                    </span>
+                </a>
+            </div>
+        </div>
+        </div>
+    </div>
 </div>
-        <?php
-        $i=1;
-        while ($row=  mysqli_fetch_array($result)){
-            echo '<tr>';
-            echo '<td>'.$i.'</td>';
-            echo "<td>" . $row["name"] . "</td>";
-            echo "<td>" . $row["email"] . "</td>";
-            echo "<td>" . $row["credit"] . "</td>";
-            echo '<td><a href=transfer.php?name='.$row['name'].'><span class="glyphicon glyphicon-plus"></span></a><td>';
-            echo '</tr>';
-            ++$i;
-        }
-        ?>
-</table>
-    </body>
     <footer>
 	<div class="container">
             <center>
